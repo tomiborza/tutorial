6561b335c1887c8b7cdc4539c9d251975ac0de53
tomiborza <borzatamas00@gmail.com>
2017-11-17 16:22:24 +0100

    renamed barmi to fizz

---
+++ barmi/Fizz/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  barmi
+//
+//  Created by Tomi Borza on 11/17/17.
+//  Copyright © 2017 Tomi Borza. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    for (int i=0; i<99; i++) {
+        cout<<"\n"<<i;
+    }
+    return 0;
+}
