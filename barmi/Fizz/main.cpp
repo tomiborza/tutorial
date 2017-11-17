1f90ffa1edeb6d4310a8bacb5ec0f006ddc33524
tomiborza <borzatamas00@gmail.com>
2017-11-17 16:39:13 +0100

    spaces added to output

--- barmi/Fizz/main.cpp
+++ barmi/Fizz/main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
     for (int i=0; i<99; i++) {
-        cout<<"\n"<<i;
+        cout<<"\n  "<<i;
     }
     return 0;
 }
